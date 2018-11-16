4106d47dde37e2e10d32c9e9179d4bd09971bc3a
admiralobvious <aferlandqc@gmail.com>
2018-11-16 10:58:50 -0500

    refactor to work with latest brevis server

--- brevis/__init__.py
+++ brevis/__init__.py
@@ -6,13 +6,13 @@ logger = logging.getLogger(__name__)
 
 
 class BrevisClient(object):
-    def __init__(self, base_url='http://localhost:1323', session=None, timeout=None):
+    def __init__(self, base_url="http://localhost:1323", session=None, timeout=None):
         self.base_url = base_url
         self.session = session or requests.Session()
         self.timeout = timeout
 
     def _make_request(self, method, endpoint, data):
-        url = '{}{}'.format(self.base_url, endpoint)
+        url = "{}{}".format(self.base_url, endpoint)
         r = requests.Request(method=method, url=url, json=data)
         p = r.prepare()
         resp = self.session.send(p, timeout=self.timeout)
@@ -27,9 +27,9 @@ class BrevisClient(object):
                 return resp.json()
             except Exception as e:
                 if resp.text:
-                    logger.error('Unhandled error {} while parsing JSON response: {}'.format(str(e), resp.text))
+                    logger.error("Unhandled error {} while parsing JSON response: {}".format(str(e), resp.text))
                 else:
-                    return ''
+                    return ""
         else:
             msg = "Got status code '{}' doing a '{}' to '{}' with body '{}".format(
                 resp.status_code, method, resp.url, resp.text)
@@ -37,21 +37,20 @@ class BrevisClient(object):
             resp.raise_for_status()
 
     def health(self):
-        return self._make_request('GET', '/', None)
+        return self._make_request("GET", "/", None)
+
+    def stats(self, id_):
+        return self._make_request("GET", "/{}/stats".format(id_), None)
 
     def shorten(self, url):
-        data = {
-            'url': url
-        }
-        return self._make_request('POST', '/shorten', data)
+        data = {"url": url}
+        return self._make_request("POST", "/shorten", data)
 
     def unshorten(self, short_url):
         if short_url.startswith(self.base_url):
             try:
-                short_url = short_url.rsplit('/', 1)[1]
+                short_url = short_url.rsplit("/", 1)[1]
             except IndexError:
                 pass
-        data = {
-            'shortUrl': short_url
-        }
-        return self._make_request('POST', '/unshorten', data)
+        data = {"short_url": short_url}
+        return self._make_request("POST", "/unshorten", data)
--- tests/test_brevis.py
+++ tests/test_brevis.py
@@ -6,38 +6,30 @@ import requests_mock
 
 class TestBrevis(unittest.TestCase):
     def setUp(self):
-        base_url = 'mock://brevis'
+        base_url = "mock://brevis"
         self.client = brevis.BrevisClient(base_url)
         self.adapter = requests_mock.Adapter()
-        self.client.session.mount('mock', self.adapter)
+        self.client.session.mount("mock", self.adapter)
 
     def test_shorten(self):
-        url = 'http://test.com'
-        data = {
-            'shortUrl': self.client.base_url + '/Ur4HRbUCN',
-            'url': url
-        }
-        self.adapter.register_uri('POST', self.client.base_url+'/shorten', json=data)
+        url = "http://test.com"
+        data = {"url": url}
+        self.adapter.register_uri("POST", self.client.base_url+"/shorten", json=data)
         resp = self.client.shorten(url)
 
         self.assertDictEqual(data, resp)
 
     def test_unshorten(self):
-        short_url = 'Ur4HRbUCN'
-        data = {
-            'shortUrl': short_url,
-            'url': 'http://test.com'
-        }
-        self.adapter.register_uri('POST', self.client.base_url+'/unshorten', json=data)
+        short_url = "Ur4HRbUCN"
+        data = {"shortUrl": short_url,}
+        self.adapter.register_uri("POST", self.client.base_url+"/unshorten", json=data)
         resp = self.client.unshorten(short_url)
 
         self.assertDictEqual(data, resp)
 
     def test_health(self):
-        data = {
-            'message': 'Brevis: URL shortener API'
-        }
-        self.adapter.register_uri('GET', self.client.base_url+'/', json=data)
+        data = {"message": "Brevis: URL shortener API"}
+        self.adapter.register_uri("GET", self.client.base_url+"/", json=data)
         resp = self.client.health()
 
         self.assertEqual(data, resp)
