d618586ead86660b3a390babde530601eb999b69
admiralobvious <aferlandqc@gmail.com>
2018-11-19 16:36:22 -0500

    up version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="brevis",
-    version="0.5.1",
+    version="0.5.2",
     description="Python client for the Brevis URL shortener API",
     url="http://github.com/admiralobvious/brevis-python-client",
     author="Alexandre Ferland",
